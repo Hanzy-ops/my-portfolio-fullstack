50619785bc334116b462a3fa13cf0f14a892f4c1
hanif <yuhuyiha136@gmail.com>
2023-12-31 00:05:16 +0700

    change name

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -2,7 +2,8 @@ import { Backend_skill, Frontend_skill, Full_stack, Other_skill, Skill_data } fr
 import Image from "next/image";
 import React from "react";
 import SkillDataProvider from "../sub/SkillDataProvider";
-import Skilltext from "../sub/Skilltext";
+import Skilltext from "../sub/SkillText";
+import SkillText from "../sub/SkillText";
 
 const Skills = () => {
   return (
@@ -11,7 +12,7 @@ const Skills = () => {
       className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
       style={{ transform: "scale(0.9)" }}
     >
-      <Skilltext />
+      <SkillText />
       <div className="flex flex-row justify-around flex-wrap  mt-4 gap-5 items-center">
         {Skill_data.map((image, index) => (
           <SkillDataProvider
--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -1,14 +1,13 @@
 "use client"
-
 import React from 'react'
 import {motion} from 'framer-motion'
 import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
 import { SparklesIcon } from '@heroicons/react/24/solid'
 
-const Skilltext = () => {
+const SkillText = () => {
   return (
     <div className='w-full h-auto flex flex-col items-center justify-center'>
-        <motion.div
+<motion.div
           variants={slideInFromTop}
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
@@ -18,19 +17,19 @@ const Skilltext = () => {
           </h1>
         </motion.div>
         <motion.div
-         variants={slideInFromLeft(0.5)}
-         className='text-[30px] text-white font-medium mt-[10px] text-center mb-[15px] '
+        variants={slideInFromLeft(0.5)}
+        className='text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]'
         >
-            Making apps modern technologies
+            Making apps with modern technologies
         </motion.div>
         <motion.div
         variants={slideInFromRight(0.5)}
-        className='cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center '
+        className='cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center'
         >
-            Never miss task deadline or idea
+            Never miss a task, deadline or idea
         </motion.div>
     </div>
   )
 }
 
-export default Skilltext
+export default SkillText
